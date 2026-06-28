3548496919fcc723807bd4d703ebc9066fe4a0f5
Servet Erdoğan <eservet224@gmail.com>
2026-06-28 15:00:45 +0300

    Add Hilt RepositoryModule for dependency injection
    - Define `RepositoryModule` to manage the injection of repository implementations using Dagger Hilt.
    - Bind `MemoryRepository` and `DailySummaryRepository` interfaces to their respective implementations.

---
+++ app/src/main/java/com/serveterdogan/lume/di/RepositoryModule.kt
@@ -0,0 +1,28 @@
+package com.serveterdogan.lume.di // veya com.serveterdogan.lume.data.di
+
+import com.serveterdogan.lume.data.repo.DailySummaryRepositoryImpl
+import com.serveterdogan.lume.data.repo.MemoryRepositoryImpl
+import com.serveterdogan.lume.domain.repository.DailySummaryRepository
+import com.serveterdogan.lume.domain.repository.MemoryRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindMemoryRepository(
+        memoryRepositoryImpl: MemoryRepositoryImpl
+    ): MemoryRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindDailySummaryRepository(
+        dailySummaryImpl: DailySummaryRepositoryImpl
+    ): DailySummaryRepository
+}
